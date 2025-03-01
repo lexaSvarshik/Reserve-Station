57185b340881cc119874d7e050e0a6d1905f0f72
31893189 <106328744+31893189@users.noreply.github.com>
2025-03-01 12:36:52 +0300

    [Feature] возврат резни после манифеста (#234)
    * give random guns after manifest
    
    * more guns
    
    * fix
    
    * fix
    
    * fix
    
    ---------
    
    Co-authored-by: 3189 <->

---
+++ Content.Shared/_White/CVars.cs
@@ -0,0 +1,80 @@
+using Robust.Shared.Configuration;
+
+namespace Content.Shared._White;
+
+[CVarDefs]
+public sealed class WhiteCVars
+{
+    #region Aspects
+
+    public static readonly CVarDef<bool> IsAspectsEnabled =
+        CVarDef.Create("aspects.enabled", false, CVar.SERVERONLY);
+
+    public static readonly CVarDef<double> AspectChance =
+        CVarDef.Create("aspects.chance", 0.1d, CVar.SERVERONLY);
+
+    #endregion
+
+    #region Locale
+
+    public static readonly CVarDef<string>
+        ServerCulture = CVarDef.Create("white.culture", "ru-RU", CVar.REPLICATED | CVar.SERVER);
+
+    #endregion
+
+    #region OptionsMisc
+
+    public static readonly CVarDef<bool> LogInChat =
+        CVarDef.Create("white.log_in_chat", true, CVar.CLIENT | CVar.ARCHIVE | CVar.REPLICATED);
+
+    #endregion
+
+    #region TTS
+
+        /// <summary>
+    /// URL of the TTS server API.
+    /// </summary>
+    public static readonly CVarDef<bool> TTSEnabled =
+        CVarDef.Create("tts.enabled", false, CVar.SERVER | CVar.REPLICATED | CVar.ARCHIVE);
+
+    /// <summary>
+    /// URL of the TTS server API.
+    /// </summary>
+    public static readonly CVarDef<string> TTSApiUrl =
+        CVarDef.Create("tts.api_url", "", CVar.SERVERONLY | CVar.ARCHIVE);
+
+    /// <summary>
+    /// Auth token of the TTS server API.
+    /// </summary>
+    public static readonly CVarDef<string> TTSApiToken =
+        CVarDef.Create("tts.api_token", "", CVar.SERVERONLY | CVar.CONFIDENTIAL);
+
+    /// <summary>
+    /// Amount of seconds before timeout for API
+    /// </summary>
+    public static readonly CVarDef<int> TTSApiTimeout =
+        CVarDef.Create("tts.api_timeout", 5, CVar.SERVERONLY | CVar.ARCHIVE);
+
+    /// <summary>
+    /// Default volume setting of TTS sound
+    /// </summary>
+    public static readonly CVarDef<float> TTSVolume =
+        CVarDef.Create("tts.volume", 0f, CVar.CLIENTONLY | CVar.ARCHIVE);
+
+    /// <summary>
+    /// Count of in-memory cached tts voice lines.
+    /// </summary>
+    public static readonly CVarDef<int> TTSMaxCache =
+        CVarDef.Create("tts.max_cache", 250, CVar.SERVERONLY | CVar.ARCHIVE);
+
+    /// <summary>
+    /// VoiceId for Announcement TTS
+    /// </summary>
+    public static readonly CVarDef<string> TTSAnnounceVoiceId =
+        CVarDef.Create("tts.announce_voice", "Announcer", CVar.SERVERONLY | CVar.ARCHIVE);
+
+    #endregion
+    
+    public static readonly CVarDef<bool> PMMEnabled =
+        CVarDef.Create("pmm.enabled", true, CVar.SERVER | CVar.ARCHIVE);
+}
